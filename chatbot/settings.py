536b78065a7bc91a0fc424ef49360235d1a2fd23
ikenagagabriel <ikenaga.gabriel@gmail.com>
2022-10-16 19:04:25 -0300

    .

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from settings_secret import *
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
